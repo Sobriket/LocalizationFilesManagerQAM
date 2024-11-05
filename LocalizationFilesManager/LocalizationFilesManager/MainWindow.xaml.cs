d1f5d37935143f1bd3084b64d94353a5d8c52fd9
Crepiliere <acrepilierepokemon@gmail.com>
2024-11-05 17:23:14 +0100

    Export CSV

--- LocalizationFilesManager/LocalizationFilesManager/MainWindow.xaml.cs
+++ LocalizationFilesManager/LocalizationFilesManager/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Data;
+using System.IO;
+using System.Reflection;
+using System.Windows;
 
 namespace LocalizationFilesManager
 {
@@ -13,6 +16,22 @@ namespace LocalizationFilesManager
             List<DataLocalization> Data = new List<DataLocalization>();
             Data.Add(new DataLocalization() { ID = "START", EN = "Start", FR = "Commencer", ES = "" });
             dataGrid.ItemsSource = Data;
+        }
+
+        private void Button_ExportCsv(object sender, RoutedEventArgs e)
+        {
+            var filepath = "test.csv";
+            // Create the CSV file to which grid data will be exported.  
+            StreamWriter sw = new StreamWriter(new FileStream(filepath, FileMode.Create, FileAccess.Write));
+
+            foreach (DataLocalization item in dataGrid.Items.OfType<DataLocalization>())
+            {
+                sw.Write(item.ID +";" + item.EN + ";" + item.FR + ";" + item.ES);
+                sw.Write(sw.NewLine);
+            }
+            sw.Close();
+
+            
         }
     }
 }
