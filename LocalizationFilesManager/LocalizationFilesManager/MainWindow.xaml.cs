c2115641879bf7c82d6978fdb768af5803e04f7b
TartieTerezi <58311742+TartieTerezi@users.noreply.github.com>
2024-11-19 11:41:37 +0100

    Ultra Shortcut

--- LocalizationFilesManager/LocalizationFilesManager/MainWindow.xaml.cs
+++ LocalizationFilesManager/LocalizationFilesManager/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System.Data;
+using System.Data.Common;
 using System.Reflection.PortableExecutable;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Input;
 
 namespace LocalizationFilesManager
 {
@@ -13,9 +15,15 @@ namespace LocalizationFilesManager
     {
         DataTable Data = new DataTable();
 
+        public static RoutedCommand shortcutSave = new RoutedCommand();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            shortcutSave.InputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+
+            CommandBindings.Add(new CommandBinding(shortcutSave, MenuItemSaveClick));
 
             Data.Columns.Clear();
             DataColumn column = Data.Columns.Add("ID");
