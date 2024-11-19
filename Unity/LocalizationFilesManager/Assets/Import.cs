dad3e4fe539376990b7981497d38d4dab0ced9fb
Maxime Chauvin <134788464+Sobriket@users.noreply.github.com>
2024-11-19 12:28:48 +0100

    xml

--- Unity/LocalizationFilesManager/Assets/Import.cs
+++ Unity/LocalizationFilesManager/Assets/Import.cs
@@ -7,6 +7,9 @@ using Newtonsoft.Json;
 using System.Globalization;
 using System.Threading;
 using System.Linq;
+using System.Xml;
+using System;
+using System.Xml.Serialization;
 
 
 public class Import : MonoBehaviour
@@ -17,13 +20,26 @@ public class Import : MonoBehaviour
     static List<string> languages = new List<string>();
     static string language = "en";
 
+    [XmlRoot("DataLocalization")]
+    public class DataLocalization
+    {
+        public DataLocalization()
+        {
+            Data = new List<List<String>>();
+        }
+
+        public List<List<String>> Data { get; set; }
+    }
+
     private void Start()
     {
-        LoadJson(path);
+       // LoadJson(path);
+        LoadXML(path);
     }
 
     private void LoadJson(string _filepath)
     {
+        data.Clear();
         string jsonString = File.ReadAllText(_filepath);
         data = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(jsonString);
 
@@ -46,6 +62,48 @@ public class Import : MonoBehaviour
         int cultureID = 0;
         Debug.Log(culture);
         //if(culture == CultureTypes.)
+    }
+
+    private void LoadXML(string _filepath)
+    {
+        data.Clear();
+        System.Xml.Serialization.XmlSerializer xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(DataLocalization));
+        DataLocalization localizationData;
+        using (StreamReader wr = new StreamReader(_filepath))
+        {
+            localizationData = (DataLocalization)xmlSerializer.Deserialize(wr);
+
+            for(int i =1; i < localizationData.Data[0].Count; i++)
+            {
+                Dictionary<string,string> tempDictionary = new Dictionary<string,string>();
+                for (int j = 0; j < localizationData.Data.Count; j++)
+                {
+                     tempDictionary.Add(localizationData.Data[j][0], localizationData.Data[j][i]);
+                }
+                data.Add(tempDictionary);
+            }
+
+           // for (int i = 0; i < localizationData.Data.Count; i++)
+           // {
+           //     AddNewLangage(localizationData.Data[i][0]);
+           // }
+
+           // for (int i = 0; i < localizationData.Data[0].Count; i++)
+           // {
+           //
+           //     string[] read = new string[localizationData.Data.Count];
+           //
+           //     for (int j = 0; j < localizationData.Data.Count; j++)
+           //     {
+           //         read[j] = localizationData.Data[j][i];
+           //     }
+           //
+           //     Data.Rows.Add(read);
+           // }
+
+        }
+
+       // dataGrid.ItemsSource = Data.DefaultView;
     }
 
     public static string GetText(string id)
